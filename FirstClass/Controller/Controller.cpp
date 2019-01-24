b9755586f29deec067e06386dcc2a985f2cd566a
David <davidlarson46@yahoo.com>
2019-01-24 09:13:32 -0700

    added a lot of input and output statements.

--- FirstClass/Controller/Controller.cpp
+++ FirstClass/Controller/Controller.cpp
@@ -8,6 +8,7 @@
 
 #include "Controller.hpp"
 
+
 Controller :: Controller()
 {
     this->specialNumber = 4527;
@@ -16,5 +17,15 @@ Controller :: Controller()
 void Controller :: start()
 {
     cout << "This is in the start method" << endl;
-    cout << "Here is the class number: " << specialNumber << " Ithink it is really cool!" << endl;  //always end these output statements with "endl". 
+    cout << "Here is the class number: " << specialNumber << " I think it is really cool!" << endl;  //always end these output statements with endl.
+    string input;
+    cout << "Type your name" << endl;
+    getline(cin, input);
+    cout << input << endl;
+    cout << "isn't it wierd that the program exit code line pops up before?" << endl;
+    cout << "Type in a number" << endl;
+    cin >> specialNumber;
+    cout << "Here is the number you typed: " << specialNumber << endl;
 }
+
+
